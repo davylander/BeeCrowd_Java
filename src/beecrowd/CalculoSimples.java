230f06cddfcfa961a657c3fddf985aac0b30657e
Davylander <davisouto.dev@gmail.com>
2022-03-17 23:01:36 -0300

    Continuando solução de string com array. Precisa criar variaveis para receber cada item dos arrays, e depois transformar essas Strings em int ou double. após isso, fazer as operaçõs que o problema pede

--- src/beecrowd/CalculoSimples.java
+++ src/beecrowd/CalculoSimples.java
@@ -7,24 +7,21 @@ public class CalculoSimples {
 		Scanner scanner = new Scanner(System.in);
 		
 		String linha1 = scanner.nextLine();
-		System.out.println(linha1);
 		String linha2 = scanner.nextLine();
-		System.out.println(linha2);
 		
 		String[] arrayValores1 = linha1.split(" ");
 		String[] arrayValores2 = linha2.split(" ");
 		
-		/*for(int i=0; i<arrayValores1.length; i++) {
-			System.out.println();
-			for(int j=0; j<arrayValores2.length; j++) {
-				System.out.println(arrayValores1[i]);
-				System.out.println(arrayValores2[j]);
-			}
+		for(int i=0; i<arrayValores1.length; i++) {
+			System.out.print(" " + arrayValores1[i] + "");
 	
 		}
-		*/
 		
-
+		System.out.println();
+		
+		for(int i=0; i<arrayValores2.length; i++) {
+			System.out.print(" " + arrayValores2[i] + " ");
+		}
 	}
 
 }
