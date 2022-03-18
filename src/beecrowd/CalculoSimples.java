1ecbfb77f054583a36692e1ad8b3aea8d664b947
Davylander <davisouto.dev@gmail.com>
2022-03-18 01:49:40 -0300

    amanha enviar problema Calculo Simples

--- src/beecrowd/CalculoSimples.java
+++ src/beecrowd/CalculoSimples.java
@@ -12,6 +12,19 @@ public class CalculoSimples {
 		String[] arrayValores1 = linha1.split(" ");
 		String[] arrayValores2 = linha2.split(" ");
 		
+		//linha 1
+		int numero1 = Integer.parseInt(arrayValores1[1]);
+		double valor1 = Double.parseDouble(arrayValores1[2]);
+		//linha 2
+		
+		int numero2 = Integer.parseInt(arrayValores2[1]);
+		double valor2 = Double.parseDouble(arrayValores2[2]);
+		
+		double valor = (numero1 * valor1) + (numero2 * valor2);
+		
+		System.out.printf("VALOR A PAGAR: R$ %.2f\n", valor);
+		
+		/*
 		for(int i=0; i<arrayValores1.length; i++) {
 			System.out.print(" " + arrayValores1[i] + "");
 	
@@ -22,6 +35,7 @@ public class CalculoSimples {
 		for(int i=0; i<arrayValores2.length; i++) {
 			System.out.print(" " + arrayValores2[i] + " ");
 		}
+		*/
 	}
 
 }
