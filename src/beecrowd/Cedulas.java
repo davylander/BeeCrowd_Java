e7db76c6e01ad2a46241473e3bda3a3bf8d24c8e
davylander <101679754+davylander@users.noreply.github.com>
2022-03-21 18:36:45 -0300

    Iniciante 1018: Cédulas (Em andamento)

--- src/beecrowd/Cedulas.java
+++ src/beecrowd/Cedulas.java
@@ -1,5 +1,7 @@
 package beecrowd;
 
+import java.util.Scanner;
+
 public class Cedulas {
 
 	public static void main(String[] args) {
@@ -12,6 +14,45 @@ public class Cedulas {
 		 * Saída: Imprima o valor lido e, em seguida, a quantidade mínima de notas de cada tipo necessárias, conforme o exemplo fornecido. 
 		 * Não esqueça de imprimir o fim de linha após cada linha, caso contrário seu programa apresentará a mensagem: “Presentation Error”.
 		 */
+		
+		// dividir o valor, e usar o resto para a proxima divisão
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int n = scanner.nextInt();
+		
+		int r100 = n % 100;
+		int r50 = r100 % 50;
+		int r20 = r50 % 20;
+		int r10 = r20 % 10;
+		int r5 = r10 % 5;
+		int r2 = r5 % 2;
+		int r1 = r2 % 1;
+		
+		int n100 = n / 100;
+		int n50 = r100 / 50;
+		int n20 = r50 / 20;
+		int n10 = r20 / 10;
+		int n5 = r10 / 5;
+		int n2 = r5 / 2;
+		int n1 = r2 / 1;
+		
+		System.out.println(r100);
+		System.out.println(r50);
+		System.out.println(r20);
+		System.out.println(r10);
+		System.out.println(r5);
+		System.out.println(r2);
+		System.out.println(r1);
+		System.out.println("_______________________________");
+		System.out.println(n100);
+		System.out.println(n50);
+		System.out.println(n20);
+		System.out.println(n10);
+		System.out.println(n5);
+		System.out.println(n2);
+		System.out.println(n1);
+		
 		
 	}
 
