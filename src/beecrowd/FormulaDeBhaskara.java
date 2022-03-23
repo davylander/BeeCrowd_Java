add2d6b72cee281b48f91ab99d29e127fb4fb0d2
davylander <101679754+davylander@users.noreply.github.com>
2022-03-23 20:03:58 -0300

    Iniciante 1036: Fórmula de Bháskara (Iniciando)

---
+++ src/beecrowd/FormulaDeBhaskara.java
@@ -0,0 +1,32 @@
+/*
+ * Classe da Fórmula de Bháskara
+ * 23/03/2022
+ * Todos os direitos autorais para Davi Souto Neri
+ */
+
+package beecrowd;
+
+/*
+ * @version 1.0
+ * @author Davi Souto Neri - BeeCrowd 
+ */
+
+public class FormulaDeBhaskara {
+
+	public static void main(String[] args) {
+		/*
+		 * Leia 3 valores de ponto flutuante e efetue o cálculo das raízes da equação de Bhaskara. Se não for possível calcular as raízes, 
+		 * mostre a mensagem correspondente “Impossivel calcular”, caso haja uma divisão por 0 ou raiz de numero negativo.
+		 * 
+		 * Entrada: Leia três valores de ponto flutuante (double) A, B e C.
+		 * 
+		 * Saída: Se não houver possibilidade de calcular as raízes, apresente a mensagem "Impossivel calcular". Caso contrário, imprima o resultado das raízes 
+		 * com 5 dígitos após o ponto, com uma mensagem correspondente conforme exemplo abaixo. Imprima sempre o final de linha após cada mensagem.
+		 */
+		//Formulas: https://www.estudopratico.com.br/wp-content/uploads/2014/11/formula-de-bhaskara-origem-importancia-e-exemplos-1200x675.jpg
+		// Stackoverflow: https://pt.stackoverflow.com/questions/316798/fórmula-de-bhaskara-em-c-com-o-vs-2017
+		
+
+	}
+
+}
